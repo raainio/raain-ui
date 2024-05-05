f114205977c36056c275a350f118cc339dbfadeb
Mat L <mat@mlefree.com>
2024-05-05 10:34:25 +0200

    fix transparency

--- src/factories/CompareElement.ts
+++ src/factories/CompareElement.ts
@@ -27,8 +27,8 @@ export class CompareElement {
                 {
                     type: 'bubble',
                     data: inputs.points,
-                    borderColor: Tools.getTransparency(ChartColors.grey, 0.9),
-                    backgroundColor: Tools.getTransparency(ChartColors.grey, 0.9),
+                    borderColor: Tools.getTransparency(ChartColors.grey, 0.7),
+                    backgroundColor: Tools.getTransparency(ChartColors.grey, 0.7),
                     pointStyle: 'circle',
                     pointHoverRadius: 15,
                 },
