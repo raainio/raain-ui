99b3c9d7156eafceaa3a439e6a502b0d6b679e82
Mat L <mat@mlefree.com>
2024-11-21 16:18:36 +0100

    GH CI test local date

--- specs/factories/Tools.spec.ts
+++ specs/factories/Tools.spec.ts
@@ -9,7 +9,7 @@ describe('Factories.Tools', () => {
         expect(Tools.formatDate(new Date(0), DateRange.YEAR)).eq('1970');
         expect(Tools.formatDate(new Date(0), DateRange.MONTH)).eq('1970-01');
         expect(Tools.formatDate(new Date(0), DateRange.DAY)).eq('1970-01-01');
-        expect(Tools.formatDate(new Date(0), DateRange.HOUR)).eq('1970-01-01 01h');
-        expect(Tools.formatDate(new Date(0), DateRange.MINUTE)).eq('1970-01-01 01:00');
+        expect(Tools.formatDate(new Date(0), DateRange.HOUR)).contains('1970-01-01 0'); // 01h depending on local
+        expect(Tools.formatDate(new Date(0), DateRange.MINUTE)).contains('1970-01-01 0'); // 01:00 depending on local
     });
 });
